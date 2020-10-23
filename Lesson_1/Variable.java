ac37386f53fc542d99b4af6b956e424c98fd556a
Sergey Gr <leshiysv85@gmail.com>
2020-10-23 18:24:01 +0300

    bug fixed for Variable

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
 	public static void main(String[] args) {
-	byte cores = 2;
-	short battery = 6300;
-	int hDriveMb = 131072;
-	long hDriveByte = 134_217_728;
-	float freqncy= 1.6f;
-	double graphicsKb = 0.000022888183594d;
-	char firstLetter= 'a';
-	boolean laptop = true;
-	
-	System.out.println(cores);
-	System.out.println(battery);
-	System.out.println(hDriveMb);
-	System.out.println(hDriveByte);
-	System.out.println(freqncy);
-	System.out.println(graphicsKb);
-	System.out.println(firstLetter);
-	System.out.println(laptop);
+		byte cores = 2;
+		short battery = 6300;
+		int hDriveMb = 131072;
+		long hDriveByte = 134_217_728;
+		float freqncy = 1.6f;
+		double graphicsKb = 0.000022888183594d;
+		char firstLetter = 'a';
+		boolean laptop = true;
+		
+		System.out.println(cores);
+		System.out.println(battery);
+		System.out.println(hDriveMb);
+		System.out.println(hDriveByte);
+		System.out.println(freqncy);
+		System.out.println(graphicsKb);
+		System.out.println(firstLetter);
+		System.out.println(laptop);
 	}
 }
