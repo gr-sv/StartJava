3cd5018ff071b0fd6af1fe2da0dbbccf7d8720af
Sergey Gr <leshiysv85@gmail.com>
2020-10-28 18:04:21 +0300

    Implement HW 1.8

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,26 @@
+public class Cycle {
+	public static void main(String[] args) {
+		System.out.println("Cycle For:");
+		for(int i = 0; i <= 20; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("Cycle While:");
+		int j = 6;
+		while(j >= -6) {
+			System.out.println(j);
+			j = j - 2;
+		}
+
+		System.out.println("Cycle Do... while:");
+		int k = 10;
+		int sum = 0;
+		do {
+			if(k % 2 != 0) {
+				sum = sum + k;
+			}
+			k++;
+		} while (k < 20);
+		System.out.println("Cумма нечетных чисел равна " + sum);
+	}
+}
