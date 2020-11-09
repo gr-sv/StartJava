b8edb718f626385abf77d9713d9df4b850d85d0b
Sergey Gr <leshiysv85@gmail.com>
2020-11-09 18:15:23 +0300

    Bug fixed for HW_MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,25 +1,22 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
 		int compNumber = 38;
-		int myNumber = 52;
+		int myNumber = 45;
 
 		if (0 <= myNumber && myNumber <= 100) {
 			System.out.println("Играем!");
-			System.out.println("Ваше число " + myNumber);
 
 			if (myNumber > compNumber) {
 				while (myNumber > compNumber) {
 					System.out.println("Ваше число больше, чем число, которое загадал компьютер.");
-					myNumber -= 1;
-					System.out.println("Ваше число " + myNumber);
+					myNumber --;
 				}
 			}
 
 			if (myNumber < compNumber) {
 				while (myNumber < compNumber) {
 					System.out.println("Ваше число меньше, чем число, которое загадал компьютер.");
-					myNumber += 1;
-					System.out.println("Ваше число " + myNumber);
+					myNumber ++;
 				}
 			}
 
