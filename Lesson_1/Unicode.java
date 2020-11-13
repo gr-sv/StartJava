a473a9e9e9906e88891238d4b8fab52cff2a78b6
Sergey Gr <leshiysv85@gmail.com>
2020-11-13 10:46:50 +0300

    Bug fixed for HW_Unicode

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,7 @@
+public class Unicode {
+	public static void main(String[] args) {
+		for (int unicodeNumber = 9398; unicodeNumber <= 10178; unicodeNumber++) {
+			System.out.println((char) unicodeNumber);
+		}
+	}
+}
