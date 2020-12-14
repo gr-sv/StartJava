ec53ccca8e1c8829dbc244e83ad88d8c84c2e534
Sergey Gr <leshiysv85@gmail.com>
2020-12-14 19:16:16 +0300

    Modified HW_Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -8,68 +8,47 @@ public class Jaeger {
 	private int strength;
 	private int armor;
 
-	public String getModelName() {
-		return modelName;
+	public Jaeger(String modelName, String mark, String country, float height, float weight, int speed, int strength, int armor) {
+		this.modelName = modelName;
+		this.mark = mark;
+		this.country = country;
+		this.height = height;
+		this.weight = weight;
+		this.speed = speed;
+		this.strength = strength;
+		this.armor = armor;
 	}
 
-	public void setModelName(String modelName) {
-		this.modelName = modelName;
+	public String getModelName() {
+		return modelName;
 	}
 
 	public String getMark() {
 		return mark;
 	}
 
-	public void setMark(String mark) {
-		this.mark = mark;
-	}
-
 	public String getCountry() {
 		return country;
-	}
-
-	public void setCountry (String country) {
-		this.country = country;
 	}
 
 	public float getHeight() {
 		return height;
 	}
 
-	public void setHeight(float height) {
-		this.height = height;
-	}
-
 	public float getWeight() {
 		return weight;
-	}
-
-	public void setWeight(float weight) {
-		this.weight = weight;
 	}
 
 	public int getSpeed() {
 		return speed;
 	}
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-
 	public int getStrength() {
 		return strength;
 	}
 
-	public void setStrength(int strength) {
-		this.strength = strength;
-	}
-
 	public int getArmor() {
 		return armor;
-	}
-
-	public void setArmor(int armor) {
-		this.armor = armor;
 	}
 
 	public boolean drift() {
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,24 +1,13 @@
 public class JaegerTest {
 	public static void main(String[] args) {
-		Jaeger jaegerOne = new Jaeger();
-		Jaeger jaegerTwo = new Jaeger();
-
 		System.out.println("Jaeger one");
-
-		jaegerOne.setModelName("Tacit Ronin");
-		jaegerOne.setMark("Mark-1");
-		jaegerOne.setCountry("Japan");
-		jaegerOne.setHeight(74.37f);
-		jaegerOne.setWeight(7.45f);
-		jaegerOne.setSpeed(8);
-		jaegerOne.setStrength(7);
-		jaegerOne.setArmor(3);
+		Jaeger jaegerOne = new Jaeger("Tacit Ronin", "Mark-1", "Japan", 74.37f, 7.45f, 8, 7, 3);
 
 		System.out.println("Model name: " + jaegerOne.getModelName());
 		System.out.println("Mark: " + jaegerOne.getMark());
 		System.out.println("Country: " + jaegerOne.getCountry());
 		System.out.println("Height: " + jaegerOne.getHeight() + " m");
-		System.out.println("Weight: " + jaegerOne.getWeight() + " tons");
+		System.out.println("Weight: " + jaegerOne.getWeight() + " thousand tons");
 		System.out.println("Speed: " + jaegerOne.getSpeed());
 		System.out.println("Strength: " + jaegerOne.getStrength());
 		System.out.println("Armor: " + jaegerOne.getArmor());
@@ -29,21 +18,13 @@ public class JaegerTest {
 		System.out.println(jaegerOne.powerMove());
 
 		System.out.println("Jaeger two");
-
-		jaegerTwo.setModelName("Gypsy Danger");
-		jaegerTwo.setMark("Mark-3");
-		jaegerTwo.setCountry("USA");
-		jaegerTwo.setHeight(79.25f);
-		jaegerTwo.setWeight(1.98f);
-		jaegerTwo.setSpeed(7);
-		jaegerTwo.setStrength(8);
-		jaegerTwo.setArmor(6);
+		Jaeger jaegerTwo = new Jaeger("Gypsy Danger", "Mark-3", "USA", 79.25f, 1.98f, 7, 8, 6);
 
 		System.out.println("Model name: " + jaegerTwo.getModelName());
 		System.out.println("Mark: " + jaegerTwo.getMark());
 		System.out.println("Country: " + jaegerTwo.getCountry());
 		System.out.println("Height: " + jaegerTwo.getHeight() + " m");
-		System.out.println("Weight: " + jaegerTwo.getWeight() + " tons");
+		System.out.println("Weight: " + jaegerTwo.getWeight() + " thousand tons");
 		System.out.println("Speed: " + jaegerTwo.getSpeed());
 		System.out.println("Strength: " + jaegerTwo.getStrength());
 		System.out.println("Armor: " + jaegerTwo.getArmor());
@@ -52,7 +33,5 @@ public class JaegerTest {
 		jaegerTwo.move();
 		jaegerTwo.joinTheBattle();
 		System.out.println(jaegerTwo.powerMove());
-
-
 	}
 }
